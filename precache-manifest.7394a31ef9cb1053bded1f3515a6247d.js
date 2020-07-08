5e1c0c1189c5c99b62395dd92dbd32b64e81f693
Tuklal Poudel <tuklal@pegotec.net>
2020-07-08 12:29:08 +0545

    Updates

---
+++ precache-manifest.7394a31ef9cb1053bded1f3515a6247d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c86629819a10dc8a6d3893e3dd21383",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aa66ae86946f34be34e7",
+    "url": "/monsters-rolodex/static/css/main.35d927dd.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa66ae86946f34be34e7",
+    "url": "/monsters-rolodex/static/js/main.b10036f3.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
